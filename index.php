d36b2b24ba8e022dcadb9e4c08769ff91dd66405
Jennifer Ngene <jngene@u.rochester.edu>
2019-09-18 18:26:54 -0400

    Got inc in HTML to work and removed class "bottom"
    The footer inc were placed in the wrong spot so the class "bottom" had to be deleted.

--- index.php
+++ index.php
@@ -11,24 +11,22 @@
 	<body class="container1">
 		<main>
 			<div class="top">
-				<p>Hello World! Welcome to our smash website, Junting Chen is our Information Architect, Jialin Huo is our Visual Designer, and Jennifer is our Technical Coder. Click on the links to learn more about us.</p>
+				<p>Hello World! Welcome to our smash website! Junting Chen is our Information Architect, Jialin Huo is our Visual Designer, and Jennifer is our Technical Coder. Click on the links to learn more about us.</p>
 			</div>
 			<div class="vanessa">
 				<a href="vanessa.php">
-				<img src="images/vanessa.jpg" alt="vanessa">
+				<img src="images/vanessa.jpg" alt="Vanessa Acevedo">
 			</div>
 			<div class="alyssa">
 				<a href="alyssa.php">
-				<img src="images/alyssa.jpg" alt="alyssa">
+				<img src="images/alyssa.jpg" alt="Alyssa Nelson">
 			</div>
 			<div class="kat">
 				<a href="kat.php">
-				<img src="images/kat.jpg" alt="kat">
-			</div>
-			<div class="bottom">
-				<?php include "inc/footer.php"; ?>
+				<img src="images/kat.jpg" alt="Kat Fuss">
 			</div>
 		</main>
 
 
-<?php include "inc/main-nav.inc"; ?>
+	<?php include "inc/main-nav.inc"; ?>
+	<?php include "inc/footer.inc"; ?>
