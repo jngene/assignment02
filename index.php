3cce5e2e60ef5f5746dbad61e6dfe3d58a099458
Jialin Huo <jhuo3@u.rochester.edu>
2019-09-21 14:23:53 -0400

    format + add home page image

--- index.php
+++ index.php
@@ -8,20 +8,36 @@
 		<link rel="stylesheet" href="css/styles.css">
 	</head>
 
-	<body class="container1">
+	<body>
+		<div class="header">
+		<div class="container1">
+		<header>
+			<a href="index.php">
+				<h1>Team Seattle</h1>
+				<img src="images/seattle.jpeg" alt="seattle">
+			</a>
+			<p>Hello World! Welcome to our smash website! Junting Chen is our Information Architect, Jialin Huo is our Visual Designer, and Jennifer is our Technical Coder. Click on the links to learn more.</p>
+		</header>
+		
+		</div><!--.container1-->
+
+		</div><!--.header-->
+
 		<main>
 			<div class="top">
-				<p>Hello World! Welcome to our smash website! Junting Chen is our Information Architect, Jialin Huo is our Visual Designer, and Jennifer is our Technical Coder. Click on the links to learn more about us.</p>
 			</div>
 			<div class="vanessa">
+				<h2>Vanessa Acevedo</h2>
 				<a href="vanessa.php">
 				<img src="images/vanessa.jpg" alt="Vanessa Acevedo">
 			</div>
 			<div class="alyssa">
+				<h2>Alyssa Nelson</h2>
 				<a href="alyssa.php">
 				<img src="images/alyssa.jpg" alt="Alyssa Nelson">
 			</div>
 			<div class="kat">
+				<h2>Kat Fuss</h2>
 				<a href="kat.php">
 				<img src="images/kat.jpg" alt="Kat Fuss">
 			</div>
